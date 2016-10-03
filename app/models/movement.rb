8d8e413c1284d26687a250f4df261df23c5bead9
Pachi <chiara.palmieri@gmail.com>
2016-10-03 12:45:06 +0200

    percentuale su differenza anno su anno

--- app/models/movement.rb
+++ app/models/movement.rb
@@ -1,6 +1,6 @@
 class Movement < ActiveRestClient::Base
   base_url PUBBLICALO['api_server_url']
-  
+
   get :all, "/all.json"
   get :find_in_batches, "/accounts.json"
 #  get :sublevel, "/accounts/"
@@ -24,7 +24,9 @@ class Movement < ActiveRestClient::Base
     if importo_prec.nil?
       return nil
     else
-      return ((importo.to_f * 100) / importo_prec.to_f).round(2)
+      diff = importo.to_f - importo_prec.to_f
+#      return ((importo.to_f * 100) / importo_prec.to_f).round(2)
+      return ((diff.to_f * 100) / importo_prec.to_f).round(2)
     end
   end
 
