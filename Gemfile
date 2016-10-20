3750897f6a371532d519538ca87096ba5ce00177
Pachi <chiara.palmieri@gmail.com>
2016-10-20 17:30:10 +0200

    api gateway

--- Gemfile
+++ Gemfile
@@ -45,7 +45,8 @@ end
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
-
-gem 'active_rest_client'
+gem 'flexirest'
 gem 'bootstrap-sass'
 gem 'd3-rails'
+#gem 'oauth2'
+gem 'api-auth'
--- app/models/movement.rb
+++ app/models/movement.rb
@@ -1,5 +1,6 @@
-class Movement < ActiveRestClient::Base
-  base_url PUBBLICALO['api_server_url']
+class Movement < Flexirest::Base
+  base_url         PUBBLICALO['base_url']
+  before_request :api_authenticate
 
   get :all, "/all.json"
   get :find_in_batches, "/accounts.json"
@@ -32,6 +33,28 @@ class Movement < ActiveRestClient::Base
 
   def perc_su_tot(cumulato_sup)
     return ((importo.to_f * 100) / cumulato_sup.to_f).round(2)
+  end
+
+  private
+
+  def api_authenticate (name, request)
+    site_path      = PUBBLICALO['site_path']
+    token_url      = PUBBLICALO['token_url']
+    client_id      = PUBBLICALO['client_id']
+    client_secret  = PUBBLICALO['client_secret']
+
+#    client = OAuth2::Client.new(client_id, client_secret, :site => site_path, :token_url => token_url)
+#    code   = client.client_credentials.authorization(client_id, client_secret)
+#    token  = client.get_token(:headers => {'Authorization' => code},
+#                              :content_type => 'application/x-www-form-urlencoded',
+#                              :grant_type => 'client_credentials')
+#    response = token.get(request.base_url+request.url, request.get_params)
+
+
+    api_auth_credentials(client_id, client_secret, digest: "sha256")
+
+    secret_key = ApiAuth.generate_secret_key
+    head(:unauthorized) unless ApiAuth.authentic?(request, secret_key)
   end
 
 end
